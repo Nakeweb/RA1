729a00094fcb00d3698c45b814a25b51ce38989f
Nakeweb <yamilkaled@gmail.com>
2026-09-11 19:34:08 -0500

    Delete clase1.py

--- clase1.py
+++
@@ -1,8 +0,0 @@
-n_switch = str(input("ingrese el nombre de switch"))
-print("El nombre del switch es: ", n_switch)
-
-ip_Admin = str(input("ingrese la ip de admin"))
-print("La ip de admin es: ", ip_Admin)
-
-n_Puertos = str(input("ingrese el numero de puertos: "))
-print("El numero de puertos es: ", n_Puertos)
